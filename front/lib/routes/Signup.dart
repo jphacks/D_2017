8062888dbe170f82bfd55172203fd0ccbd2d78e8
Kanon <kanon.amnos24@gmail.com>
2020-11-07 00:00:42 +0900

    Cognitoに対応させないとね

--- front/lib/routes/Signup.dart
+++ front/lib/routes/Signup.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
 import 'UserMenu.dart';
 import '../main.dart';
+import 'package:amazon_cognito_identity_dart_2/cognito.dart';
+
+/* ここ変更してね */
+final userPool = new CognitoUserPool('ap-northeast-1_XoqXlafQm', '1e7ri3io3j0ds336077gelhhub');
 
 class Signup extends StatelessWidget {
   final _formKey = GlobalKey<FormState>();
-  String _userName = '';
-  String _mailAddress = '';
-  String _password = '';
+
+  final _mailAddressController = TextEditingController();
+  final _passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -24,26 +28,6 @@ class Signup extends StatelessWidget {
               padding: const EdgeInsets.only(top: 10.0, right: 30.0, bottom: 10.0, left: 30.0),
               child: TextFormField(
                 decoration: const InputDecoration(
-                  icon: Icon(Icons.verified_user_sharp),
-                  labelText: "名前", // ラベル
-                  hintText: 'User Name', // 入力ヒント
-                ),
-                autovalidate: false, // 入力変化しても自動でチェックしない。trueにすると初期状態および入力が変化する毎に自動でvalidatorがコールされる
-                validator: (value) { // _formKey.currentState.validate()でコールされる
-                  if (value.isEmpty) {
-                    return '入力してください'; // エラー表示のメッセージを返す
-                  }
-                  return null; // 問題ない場合はnullを返す
-                },
-                onSaved: (value) => () { // this._formKey.currentState.save()でコールされる
-                  _userName = value;
-                },
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 10.0, right: 30.0, bottom: 10.0, left: 30.0),
-              child: TextFormField(
-                decoration: const InputDecoration(
                   icon: Icon(Icons.email),
                   labelText: "メールアドレスを入力", // ラベル
                   hintText: 'Mail Address', // 入力ヒント
@@ -55,9 +39,7 @@ class Signup extends StatelessWidget {
                   }
                   return null; // 問題ない場合はnullを返す
                 },
-                onSaved: (value) => () { // this._formKey.currentState.save()でコールされる
-                  _mailAddress = value;
-                },
+                controller: _mailAddressController,
               ),
             ),
             Padding(
@@ -76,9 +58,7 @@ class Signup extends StatelessWidget {
                   }
                   return null; // 問題ない場合はnullを返す
                 },
-                onSaved: (value) => () { // this._formKey.currentState.save()でコールされる
-                  _password = value;
-                },
+                controller: _passwordController,
               ),
             ),
             Align(
@@ -87,12 +67,7 @@ class Signup extends StatelessWidget {
                 onPressed: () {
                   // 各Fieldのvalidatorを呼び出す
                   if (_formKey.currentState.validate()) {
-                    // 入力データが正常な場合の処理
-                    this._formKey.currentState.save();
-                    Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => UserMenu()));
+                    _signUp(context);
                   }
                 },
                 child: Text('送信'),
@@ -129,5 +104,127 @@ class Signup extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  void _signUp(BuildContext context) async {
+    try {
+      CognitoUserPoolData userPoolData = await userPool.signUp(
+          _mailAddressController.text, _passwordController.text);
+      Navigator.push(
+        context,
+        new MaterialPageRoute<Null>(
+          settings: const RouteSettings(name: "/ConfirmRegistration"),
+          builder: (BuildContext context) => ConfirmRegistration(userPoolData),
+        ),
+      );
+    } on CognitoClientException catch (e) {
+      await showDialog<int>(
+        context: context,
+        barrierDismissible: false,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('エラー'),
+            content: Text(e.message),
+            actions: <Widget>[
+              FlatButton(
+                child: Text('OK'),
+                onPressed: () => Navigator.of(context).pop(1),
+              ),
+            ],
+          );
+        },
+      );
+    }
+  }
+}
+
+class ConfirmRegistration extends StatelessWidget {
+  final _registrationController = TextEditingController();
+  final CognitoUserPoolData _userPoolData;
+
+  ConfirmRegistration(this._userPoolData);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('レジストレーションキー確認'),
+      ),
+      body: Center(
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(_userPoolData.user.username),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'レジストレーションコード',
+                    labelText: 'レジストレーションコード',
+                  ),
+                  obscureText: true,
+                  controller: _registrationController,
+                ),
+              ),
+              Container(
+                alignment: Alignment.centerRight,
+                padding: const EdgeInsets.only(right: 8.0),
+                child: RaisedButton(
+                  child: Text('確認'),
+                  color: Colors.indigo,
+                  shape: StadiumBorder(),
+                  textColor: Colors.white,
+                  onPressed: () => _confirmRegistration(context),
+                ),
+              ),
+            ]),
+      ),
+    );
+  }
+
+  void _confirmRegistration(BuildContext context) async {
+    try {
+      await _userPoolData.user
+          .confirmRegistration(_registrationController.text);
+      await showDialog<int>(
+        context: context,
+        barrierDismissible: false,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('登録完了'),
+            content: Text('ユーザーの登録が完了しました。'),
+            actions: <Widget>[
+              FlatButton(
+                child: Text('OK'),
+                onPressed: () =>
+                    Navigator.of(context).popUntil(ModalRoute.withName('/')),
+              ),
+            ],
+        );
+        },
+      );
+    } on CognitoClientException catch (e) {
+      await showDialog<int>(
+        context: context,
+        barrierDismissible: false,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('エラー'),
+            content: Text(e.message),
+            actions: <Widget>[
+              FlatButton(
+                child: Text('OK'),
+                onPressed: () => Navigator.of(context).pop(1),
+              ),
+            ],
+          );
+        },
+      );
+    }
   }
 }
