c862cd3d4a21bb9e8fd6c03e5fdc116954fc97fb
kanon-tencho <kanon@kanon-k4.com>
2020-11-27 14:20:34 +0900

    create swift project

---
+++ swift/recovery/recovery/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  recovery
+//
+//  Created by kanon on 2020/11/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("This is ReCOVery App")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swift/recovery/recovery/recoveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  recoveryApp.swift
+//  recovery
+//
+//  Created by kanon on 2020/11/27.
+//
+
+import SwiftUI
+
+@main
+struct recoveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
