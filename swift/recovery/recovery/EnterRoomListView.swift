064a592e371aee7f37f4cdb956e5c9478897da46
kanon-tencho <kanon@kanon-k4.com>
2020-11-27 23:08:28 +0900

    fix

--- swift/recovery/recovery/EnterRoomListView.swift
+++ swift/recovery/recovery/EnterRoomListView.swift
@@ -9,7 +9,16 @@ import SwiftUI
 
 struct EnterRoomListView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            VStack {
+                Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                Slider(value: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant(10)/*@END_MENU_TOKEN@*/)
+                    .padding()
+            }
+            Toggle(isOn: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Is On@*/.constant(true)/*@END_MENU_TOKEN@*/) {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Label@*/Text("Label")/*@END_MENU_TOKEN@*/
+            }.padding()
+        }
     }
 }
 
